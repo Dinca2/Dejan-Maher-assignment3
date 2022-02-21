afc2a0c8b23a2a4a775b3c250b8ca857d83eb163
Dinca2 <82816428+Dinca2@users.noreply.github.com>
2022-02-21 12:47:48 -0500

    Create DoublyLinkedList.h

---
+++ DoublyLinkedList/DoublyLinkedList.h
@@ -0,0 +1,58 @@
+#ifndef DoublyLinkedList_h
+#define DoublyLinkedList_h
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+
+using namespace std;
+
+template<class T> struct NodeType {
+  T data; 
+  NodeType<T> *next;   
+  NodeType<T> *back;
+};
+
+template<class T> class DoubleLinkedList {
+  private:
+  
+  NodeType<T> *head; 
+  NodeType<T> *tail;
+  
+  public:
+  
+  DoublyLinkedList(); 
+//Post-Condition: the list is created. 
+ 
+~DoublyLinkedList(); 
+//Pre-Condition: the list is created. 
+//Post-Condition: all nodes are freed. 
+ 
+void insertItem(T &item);
+//Pre-Condition: the list exists and item is initialized 
+//Post-Condition: the item is inserted into the list, maintaining sorted order.  
+ 
+void deleteItem(T &item);
+//Pre-Condition: the list exists and item is initialized. 
+//Post-Condition: the node that contains item is removed from the list. If the item is 
+//not present in the list, print the message that is shown in the example output. 
+ 
+int lengthIs() const;
+//Pre-Condition: the list exists. 
+//Post-Condition: return the length instance variable. 
+ 
+void print();  
+//Pre-Condition: the list exists. 
+//Post-Condition: items in the list are printed to standard output. 
+ 
+void printReverse();  
+//Pre-Condition: the list exists. 
+//Post-Condition: items in the list are printed to standard output in reverse order. 
+
+void deleteSubsection(int upperBound, int lowerBound);
+
+T mode();
+
+void swapAlternate();
+
+};
